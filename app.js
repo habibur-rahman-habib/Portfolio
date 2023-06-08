1c87f2397a38b1808597398bdb31432b74cfc322
habibur-rahman-habib <habibur0199mhr@gmail.com>
2023-06-08 11:15:26 +0600

    49.02

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const sections = document.querySelectorAll('.section');
-const sectBtns = document.querySelectorAll('.controlls');
+const sectBtns = document.querySelectorAll('.controls');
 const sectBtn = document.querySelectorAll('.control');
 const allSections = document.querySelector('.main-content');
 
